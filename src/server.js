7b727f945fd9d588b42abd4e9c84640fb2995c15
anderrf <anderson_rocha_farias@outlook.com>
2023-01-29 16:10:39 -0300

    Addition of routes, methods and responses with status codes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,30 @@
 import http from 'http';
 
+const users = [];
+var userId = 0;
+
 const server = http.createServer((request, response) => {
-    return response.end('Hello world!');
+    const {method, url} = request;
+    if(method === 'GET' && url === '/users'){
+        return response
+            .setHeader('Content-type', 'application/json')
+            .writeHead(200)
+            .end(JSON.stringify([...users]));
+    }
+    if(method === 'POST' && url === '/users'){
+        users.push({
+            id: ++userId,
+            name: 'Anderson',
+            email: 'anderson@test.com'
+        });
+        return response
+            .setHeader('Content-type', 'application/json')
+            .writeHead(201)
+            .end();
+    }
+    return response
+        .setHeader('Content-type', 'application/json')
+        .writeHead(404)
+        .end();
 });
 server.listen(3333);
